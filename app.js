fc3ee872048d7cde662e6c6c4977c7b58595ae12
Raviraj Dulange <redhood.dumb@gmail.com>
2021-05-02 09:26:14 +0530

    Redesigning

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
 const buttonColors = ['red', 'blue', 'green', 'yellow']
 const gamePattern = []
-const userClickedPattern = []
-let level = 0
-// Generating new Sequence(adding a new button to press)
+let userClickedPattern = []
+let level = -1
+
+function playSound(name) {
+    const audio = new Audio(`sounds/${name}.mp3`);
+}
+
 function nextSequence() {
-    const rand = Math.floor(Math.random() * 4);
-    let newColor = buttonColors[rand];
+    level++;
+    $('#level-title').text(`Level ${level}`);
+    const newColor = buttonColors[Math.floor(Math.random() * 4)];
     gamePattern.push(newColor);
     playSound(newColor);
-    $(`.${newColor}`).fadeOut(100).fadeIn(100);
-}
-
-$('.btn').click(function (event) {
-    console.log(event.target.id);
-    playSound(event.target.id);
-    $(`.${event.target.id}`).fadeOut(100).fadeIn(100);
-})
-
-function playSound(color) {
-    const audio = new Audio(`sounds/${color}.mp3`)
-    audio.play();
-}
-
-function newGame() {
-    let i = 0;
-    while(userClickedPattern[i] === gamePattern[i]) {
-        i++;
-    }
+    animatePress(newColor);
 }
